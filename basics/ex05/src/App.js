6860cc2abec224bb81da334860cd1a96f4ef22f1
insung93 <jis5780@gmail.com>
2021-07-22 15:37:18 +0900

    ex05 React Application Refactoring I

---
+++ basics/ex05/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export const App = function () {
+    const app = React.createElement('h1', null, 'Hello Worlddd');
+    return app;
+}
+
+
+// export default function () {
+//     const app = React.createElement('h1', null, 'Hello World');
+//     return app;
+// }
