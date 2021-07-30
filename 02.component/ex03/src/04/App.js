1e1997599aa3b2eb672e11ca529c83be4dabbcf5
insung93 <jis5780@gmail.com>
2021-07-30 11:30:42 +0900

    event #3

--- 02.component/ex03/src/04/App.js
+++ 02.component/ex03/src/04/App.js
@@ -2,9 +2,41 @@ import React, {Fragment, useRef} from 'react';
 import logo from '../assets/images/react-logo.png';
 
 export default function App() {
-    const imageRef = useRef(null);
+    // const imageRef = useRef(null);
 
+    const onKeyPressInput  = function(e) {
+        if(e.key == 'Enter') {
+            console.log(e.target.value);
+        }
+    }
+    const onFocusInput  = function(e) {
+        console.log('onFocus');
+    }
+    const onBlurInput  = function(e) {
+        console.log('onBlur');
+    }
 
+    const onMouseOverImage  = function(e) {
+        console.log('onMouseOver', `x=${e.clientX}, y=${e.clientY}`);
+    }
+    const onMouseMoveImage  = function(e) {
+        console.log('onMouseMove', `x=${e.clientX}, y=${e.clientY}`);
+    }
+    const onMouseOutImage  = function(e) {
+        console.log('onMouseOut', `x=${e.clientX}, y=${e.clientY}`);
+    }
+    const onMouseDownImage  = function(e) {
+        console.log('onMouseDown', `x=${e.clientX}, y=${e.clientY}`);
+    }
+    const onMouseUpImage  = function(e) {
+        console.log('onMouseUp', `x=${e.clientX}, y=${e.clientY}`);
+    }
+    const onClickImage  = function(e) {
+        console.log('onClick', `x=${e.clientX}, y=${e.clientY}`);
+    }
+    const onDoubleClickImage  = function(e) {
+        console.log('onDoubleClick', `x=${e.clientX}, y=${e.clientY}`);
+    }
 
     return (
         <Fragment>
@@ -18,7 +50,7 @@ export default function App() {
                 <br/>
                 <br/>
             <img
-                ref={ imageRef }
+                // ref={ imageRef }
                 style={ {
                     cursor: 'pointer',
                     width: 190,
