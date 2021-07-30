d8f9ce2b3ffd0f3556c583b0ed7171aeb437eb6d
insung93 <jis5780@gmail.com>
2021-07-30 10:08:43 +0900

    Kanban, Guestbook # css => scss

--- 02.component/guestbook/src/Message.js
+++ 02.component/guestbook/src/Message.js
@@ -1,17 +1,18 @@
 import React, {Fragment} from 'react';
+import styles from './assets/scss/Message.scss';
 
-export default function Message({ name, message }) {
+export default function Message({ no, name, message }) {
     return (
-        <li className="Guestbook__List__Item">
+        <li className={ styles.Message__List__Item }>
             <strong>{ name }</strong>
             <p>
                 { message && message.split('\n').map((line, index) => index > 0 ?
-                    <Fragment>
+                    <Fragment key={`${no}-${index}`}>
                         <br/>
                         { line }
                     </Fragment> : line) }
             </p>
-            <strong></strong>
+            <strong/>
             <a href=''>삭제</a>
         </li>
     );
