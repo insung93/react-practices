cda8019b044652d2b52e60fb6d8693280f05aad8
insung93 <jis5780@gmail.com>
2021-08-03 19:42:11 +0900

    상태컴포넌트&데이터전달#1

--- 02.component/ex04/src/02/Form.js
+++ 02.component/ex04/src/02/Form.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import './assets/Form.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import './assets/Form.css';
 
 export default function Form() {
     const [name, setName] = useState('');
@@ -11,19 +11,15 @@ export default function Form() {
     const [birthYear, setBirthYear] = useState(1984);
     const [agreeProv, setAgreeProv] = useState('no');
 
-    const onChangeInputName = function (e) {
+    const onChangeInputName = function(e) {
         // setName(e.target.value);
-        // 10자 제한
+        // 10자 제한(validation)
         setName(e.target.value.substr(0, 10));
     }
-    // const onChangeInputEmail = async function (e) {
-    //     setEmail(e.target.value);
-    //     const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    //     const result = await fetch('/user/emailcheck?email' + e.target.value);
-    //     setValidEmail(!result.data);
-    // }
-    const onChangeInputEmail = function (e) {
+
+    const onChangeInputEmail = function(e) {
         setEmail(e.target.value);
+
         const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
         setValidEmail(re.test(e.target.value));
     }
@@ -33,14 +29,15 @@ export default function Form() {
     }
 
     const onChangeInputProv = function(e) {
+        const status = e.target.value === 'no' ? 'yes' : 'no';
+        
         // API 호출
-        const status = e.target.value === 'no' ? 'yes' : 'no'
-        const url = `/prov/agree?status=${status}`;
+        const url = `/prov/agree?staus=${status}`;
         console.log(url);
-        if(false) {
+        if(false){
             setAgreeProv(status);
         }
-    }
+    }    
 
     return (
         <form id="joinForm" name="joinForm" method="post" action="/do/not/post">
@@ -50,50 +47,55 @@ export default function Form() {
                 name="name"
                 type="text"
                 value={ name }
-                onChange={ onChangeInputName } />
+                onChange={ onChangeInputName }/>
 
             <label htmlFor="email">이메일</label>
-            <input id="email" name="email" type="text" value={email} onChange={ onChangeInputEmail }/>
+            <input
+                id="email"
+                name="email"
+                type="text"
+                value={ email }
+                onChange={ onChangeInputEmail }/>
             {
-                email === '' ? 
+                email === '' ?
                     null :
-                    validEmail ? 
-                        <FontAwesomeIcon icon={ faCheckCircle } style={ {marginLeft : 5, fontSize : 14, color : 'blue'} } /> :
-                        <FontAwesomeIcon icon={ faTimesCircle } style={ {marginLeft : 5, fontSize : 14 ,color : 'red'} }/>
+                    validEmail ?
+                        <FontAwesomeIcon icon={ faCheckCircle } style={ {marginLeft: 5, fontSize: 16, color: 'blue'} } /> :
+                        <FontAwesomeIcon icon={ faTimesCircle } style={ {marginLeft: 5, fontSize: 16, color: 'red'} } />
             }
 
-
             <label htmlFor="password">패스워드</label>
-            <input id="password" name="password" type="password" value={""} />
+            <input id="password" name="password" type="password" value={ '' } />
 
             <fieldset>
                 <legend>성별</legend>
-                <label>여</label> <input type="radio" name="gender" value={"female"} checked={ gender === 'female' } onChange={ onChangeInputGender }/>
-                <label>남</label> <input type="radio" name="gender" value={"male"} checked={ gender === 'male' }  onChange={ onChangeInputGender }/>
+                <label>여</label> <input type="radio" name="gender" value={ "female" } checked={ gender === 'female' } onChange={ onChangeInputGender } />
+                <label>남</label> <input type="radio" name="gender" value={ "male" } checked={ gender === 'male' } onChange={ onChangeInputGender } />
             </fieldset>
 
             <label htmlFor="birthYear">생년</label>
-            <select id="birthYear" name='birthYear' value={birthYear} onChange= { e => setBirthYear(e.target.value)}>
-                <option value={1984}>1984년</option>
-                <option value={1985}>1985년</option>
-                <option value={1986}>1986년</option>
-                <option value={1987}>1987년</option>
-                <option value={1988}>1988년</option>
-                <option value={1989}>1989년</option>
-                <option value={1990}>1990년</option>
+            <select id="birthYear" name='birthYear' value={ birthYear } onChange={ e => setBirthYear(e.target.value) }>
+                <option value={ 1984 }>1984년</option>
+                <option value={ 1985 }>1985년</option>
+                <option value={ 1986 }>1986년</option>
+                <option value={ 1987 }>1987년</option>
+                <option value={ 1988 }>1988년</option>
+                <option value={ 1989 }>1989년</option>
+                <option value={ 1990 }>1990년</option>
             </select>
 
-            <label htmlFor="birthYear">자기소개</label>
-            <textarea id='selfDescription' name='selfDescription' value={""} />
+            <label htmlFor="selfDescription">자기소개</label>
+            <textarea id='selfDescription' name='selfDescription' value={ "" } />
 
             <fieldset>
                 <legend>약관동의</legend>
-                <input id="agree-prov"
-                type="checkbox"
-                name="agreeProv"
-                value={ agreeProv }
-                checked={ agreeProv === 'yes' }
-                onChange={ onChangeInputProv }/>
+                <input
+                    id="agree-prov" 
+                    type="checkbox" 
+                    name="agreeProv" 
+                    value= { agreeProv }
+                    checked={ agreeProv === 'yes' }
+                    onChange={ onChangeInputProv } />
                 <label>서비스 약관에 동의합니다.</label>
             </fieldset>
 
---
+++ 02.component/ex04/src/04/App.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+export default function () {
+    return (
+        <Fragment>
+            <h2>Emaillist</h2>
+        </Fragment>
+    );
+}
+
