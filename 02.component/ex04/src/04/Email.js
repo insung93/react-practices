996f97836b4fc9cbfa3424932f7f0ff9cba649b6
insung93 <jis5780@gmail.com>
2021-08-05 09:55:43 +0900

    stateful component & callback pattern #1

---
+++ 02.component/ex04/src/04/Email.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Email({ firstName, lastName, email }) {
+    return (
+        <li key={ no }>
+            {`${ firstName } ${ lastName }`}
+            <br />
+            { email }
+        </li>
+    )
+}
